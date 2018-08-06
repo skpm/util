08ca7cef1f52e7cac82313543fe8cf2a27412687
Wes Byrne <wesburn@me.com>
2018-08-06 13:29:38 -0700

    Test that nested objects are not coerced for clarity

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -28,23 +28,21 @@ test('should coerce ns to js', (context, document) => {
   let str = NSString.alloc().initWithString("a string")
   expect(typeof util.toJSObject(str)).toBe("string")
   expect(util.toJSObject(str)).toBe("a string")
-
+  // NSNumber (bool)
   let bool = NSNumber.numberWithBool(true)
   expect(typeof util.toJSObject(bool)).toBe("boolean")
   expect(util.toJSObject(bool)).toBe(true)
-
+  // NSNumber (number)
   let num = NSNumber.numberWithInteger(5)
   expect(typeof util.toJSObject(num)).toBe("number")
   expect(util.toJSObject(num)).toBe(5)
-
+  // NSictionary
   let dict = NSMutableDictionary.new()
   dict.setObject_forKey(NSString.alloc().initWithString("a string"), "str")
   dict.setObject_forKey(NSNumber.numberWithInteger(5), "num")
   obj = util.toJSObject(dict)
-
-  expect(typeof obj["num"]).toEqual("number")
-  expect(obj["num"]).toEqual(5)
-
-  expect(obj["str"]).toBe("a string")
-  expect(typeof obj["str"]).toEqual("string")
+  // Nested objects are not converted
+  expect(Number(obj["str"].isKindOfClass(NSString))).toEqual(1)
+  expect(typeof obj["str"]).toEqual("object")
+  expect(typeof obj["num"]).toBe("object")
 })
