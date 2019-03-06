226735f178efbf98ff8cc4bc1a702226f98dd4f0
mathieudutour <mathieu@dutour.me>
2019-03-06 11:54:54 -0400

    fix test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -18,6 +18,6 @@ test('should inspect', (context, document) => {
   expect(util.inspect({a: 'hello'})).toBe("{ a: 'hello' }")
   expect(util.inspect(NSMakeRange(0, 10))).toBe(`NSRange { location: 0, length: 10 }`)
   expect(util.inspect(context.command)).toBe(String(context.command))
-  expect(util.inspect(document.pages[0])).toBe(`{ type: 'Page',\n  id: '${document.pages[0].id}',\n  frame: { x: 0, y: 0, width: 0, height: 0 },\n  name: 'Page 1',\n  selected: true,\n  sharedStyleId: null,\n  layers: [] }`)
+  expect(util.inspect(document.pages[0])).toBe(`{ type: 'Page',\n  id: '${document.pages[0].id}',\n  frame: { x: 0, y: 0, width: 0, height: 0 },\n  name: 'Page 1',\n  selected: true,\n  sharedStyleId: null,\n  layers: [  ] }`)
   expect(util.inspect({ type: 'Shape', id: '61658700-4746-43A0-BE9C-5191CA209481', style: { type: 'Style', id: 'ED475674-38E5-4673-A7D0-2F0903A0E2D3',opacity: 1,borderOptions:{ startArrowhead: 'None',lineEnd: 'Butt',lineJoin: 'Miter', endArrowhead: 'None', }}})).toBe(`{ type: \'Shape\',\n  id: \'61658700-4746-43A0-BE9C-5191CA209481\',\n  style: \n   { type: \'Style\',\n     id: \'ED475674-38E5-4673-A7D0-2F0903A0E2D3\',\n     opacity: 1,\n     borderOptions: \n      { startArrowhead: \'None\',\n        lineEnd: \'Butt\',\n        lineJoin: \'Miter\',\n        endArrowhead: \'None\' } } }`)
 })
